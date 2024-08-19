77efcb79c1d209de93ff3325266fa795d309f70b
Arisha <arishaaa333@gmail.com>
2024-08-19 18:59:29 +0530

    initializing repo

---
+++ Cxeglobal/src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from "react";
+import Footer from "./app/footer/Footer";
+import Contact from "./app/contact/Contact";
+import "./App.css";
+
+function App() {
+
+  return (
+    <>
+      <Contact />
+      <Footer />
+    </>
+  );
+}
+
+export default App;
